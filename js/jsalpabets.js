6deb8d2e3b37fbc53579c53b626f5283a6c1df7e
ahmedsami14 <ahmedsami14h@gmail.com>
2019-04-06 18:53:50 +0200

    sami

---
+++ js/jsalpabets.js
@@ -0,0 +1,111 @@
+var a = new Audio();
+a.load();
+a.src = "Sounds/a.mp3";
+
+var b = new Audio();
+b.load();
+b.src = "Sounds/b.mp3";
+
+var c = new Audio();
+c.load();
+c.src = "Sounds/c.mp3";
+
+var d = new Audio();
+d.load();
+d.src = "Sounds/d.mp3";
+
+var e = new Audio();
+e.load();
+e.src = "Sounds/e.mp3";
+
+var f = new Audio();
+f.load();
+f.src = "Sounds/f.mp3";
+
+var g = new Audio();
+g.load();
+g.src = "Sounds/g.mp3";
+
+var h = new Audio();
+h.load();
+h.src = "Sounds/h.mp3";
+
+var i = new Audio();
+i.load();
+i.src = "Sounds/i.mp3";
+
+var j = new Audio();
+j.load();
+j.src = "Sounds/j.mp3";
+
+var k = new Audio();
+k.load();
+k.src = "Sounds/k.mp3";
+
+var l = new Audio();
+l.load();
+l.src = "Sounds/l.mp3";
+
+var m = new Audio();
+m.load();
+m.src = "Sounds/m.mp3";
+
+var n = new Audio();
+n.load();
+n.src = "Sounds/n.mp3";
+
+var o = new Audio();
+o.load();
+o.src = "Sounds/o.mp3";
+
+var p = new Audio();
+p.load();
+p.src = "Sounds/p.mp3";
+
+var q = new Audio();
+q.load();
+q.src = "Sounds/q.mp3";
+
+var r = new Audio();
+r.load();
+r.src = "Sounds/r.mp3";
+
+var s = new Audio();
+s.load();
+s.src = "Sounds/s.mp3";
+
+var t = new Audio();
+t.load();
+t.src = "Sounds/t.mp3";
+
+var u = new Audio();
+u.load();
+u.src = "Sounds/u.mp3";
+
+var v = new Audio();
+v.load();
+v.src = "Sounds/v.mp3";
+
+var w = new Audio();
+w.load();
+w.src = "Sounds/w.mp3";
+
+var x = new Audio();
+x.load();
+x.src = "Sounds/x.mp3";
+
+var y = new Audio();
+y.load();
+y.src = "Sounds/y.mp3";
+
+var z = new Audio();
+z.load();
+z.src = "Sounds/z.mp3";
+
+
+
+
+
+
+
+
---
+++ js/jsnumber.js
@@ -0,0 +1,39 @@
+var a0 = new Audio();
+a0.load();
+a0.src = "Sounds/0.mp3";
+
+var a1 = new Audio();
+a1.load();
+a1.src = "Sounds/1.mp3";
+
+var a2 = new Audio();
+a2.load();
+a2.src = "Sounds/2.mp3";
+
+var a3 = new Audio();
+a3.load();
+a3.src = "Sounds/3.mp3";
+
+var a4 = new Audio();
+a4.load();
+a4.src = "Sounds/4.mp3";
+
+var a5 = new Audio();
+a5.load();
+a5.src = "Sounds/5.mp3";
+
+var a6 = new Audio();
+a6.load();
+a6.src = "Sounds/6.mp3";
+
+var a7 = new Audio();
+a7.load();
+a7.src = "Sounds/7.mp3";
+
+var a8 = new Audio();
+a8.load();
+a8.src = "Sounds/8.mp3";
+
+var a9 = new Audio();
+a9.load();
+a9.src = "Sounds/9.mp3";
